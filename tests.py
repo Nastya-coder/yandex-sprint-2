4ed7cec8aa5818fb452470cee1b6a45f55f232af
Nastya A <yandex.ru>
2023-03-30 19:49:50 +0300

    Apply reviewers feedback

--- tests.py
+++ tests.py
@@ -1,54 +1,35 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
-
-@pytest.fixture(scope='function')
-def Books_Collector():
-    return BooksCollector()
 
 name_book = '1984'
 worth_name = '1989' 
     
 # Добавляем одну и ту же книгу дважды    
-def test_add_theSameBook_not_add_in_list():
+def test_add_the_same_book_not_add_in_list(Books_Collector):
     Books_Collector.add_new_book(name_book)
     Books_Collector.add_new_book(name_book)
+    assert Books_Collector.favorites == []
     assert Books_Collector.books_rating == {name_book: 1}
 
 # Проверяем рейтинг, если меньше 1:
-def test_set_rating_less_than_one_cant_set(Books_Collector):
+def test_set_book_rating_less_than_one_cant_set(Books_Collector):
     Books_Collector.add_new_book(name_book)
     Books_Collector.set_book_rating(name_book, 0)
     assert Books_Collector.favorites == []
     assert Books_Collector.books_rating == {name_book: 1}
  
  # Проверяем рейтинг, если больше 10:
-def test_set_rating_more_than_ten_cant_set(Books_Collector):
+def test_set_book_rating_more_than_ten_cant_set(Books_Collector):
     Books_Collector.add_new_book(name_book)
     Books_Collector.set_book_rating(name_book, 11)
     assert Books_Collector.favorites == []
-    assert Books_Collector.books_rating == {name_book: 10}
+    assert Books_Collector.books_rating == {name_book: 1}
  
 # Проверяем на добавление книги в избранное
 def test_add_to_favorites_add_to_favorites(Books_Collector):
